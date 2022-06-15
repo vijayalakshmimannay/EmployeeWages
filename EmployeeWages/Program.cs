96f0fe92b211bc449a86e2ba76955733a07287c8
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-15 22:39:42 +0530

    [vijayalakshmi] check whwther employee is present or not

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace employeewages
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Employeeispresentornot employeeispresentornot = new Employeeispresentornot();
+            employeeispresentornot.presentorabsent();
+        }
+    }
+}
---
+++ EmployeeWages/employeeispresentornot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewages
+{
+    class employeeispresentornot
+    {
+        public void presentorabsent()
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 0;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
