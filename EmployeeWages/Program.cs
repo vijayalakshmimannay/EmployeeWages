c385950523690f200350ad140c63623762efc939
vijayalakshmimannay <45562909+vijayalakshmimannay@users.noreply.github.com>
2022-06-15 23:30:03 +0530

    Update Program.cs

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -5,10 +5,11 @@ namespace employeewages
     {
         public static void Main(string[] args)
         {
-            employeeispresentornot employeeispresentornot = new employeeispresentornot();
-            employeeispresentornot.presentorabsent();
-            employeeispresentornot.dailyWage();
-            employeeispresentornot.addPartTimeWage();
+           
+        } totalemployeewages.ispresent value = new totalemployeewages.ispresent();
+            value.presentorabsent();
+            value.dailyWage();
+            value.addPartTimeWage();
         }
     }
 }
